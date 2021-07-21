b633eb2212cfb161505072ea7cac56c8cbe7ebfe
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-21 14:56:48 -0700

    add foundational functionality for loading webpage
    Co-Authored-By: Godfrey Owidi <godfreyowiidi@gmail.com>
    Co-authored-by: John B <simpledimplejohn@gmail.com>

---
+++ PlacesYouBeen/Controllers/PlacesController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using PlacesYouBeen.Models;
+using System.Collections.Generic;
+
+namespace PlacesYouBeen.Controllers
+{
+
+  public class PlacesController : Controller //this is the folder name it calls
+  {
+    [HttpGet("/places")]
+    public ActionResult Index()
+    {
+      List<Place> allplaces = Place.GetAll();
+      return View(allplaces);
+    }
+    
+    [HttpGet("/places/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/places")]
+    public ActionResult Create(string cityName)
+    {
+      Place myPlace = new Place(cityName);
+      return RedirectToAction("Index");
+    }
+  }
+}
---
+++ PlacesYouBeen/Startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PlacesYouBeen
+{
+  public class Startup
+  {
+    public Startup(IWebHostEnvironment env)
+    {
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(env.ContentRootPath)
+          .AddEnvironmentVariables();
+      Configuration = builder.Build();
+    }
+
+    public IConfigurationRoot Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app) //shows how to route URLs and configure exceptions
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseRouting();
+
+      app.UseEndpoints(routes => //this looks for the Home folder, Index file 
+      {
+        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Hey your request did not work--check your GET POST!");
+      });
+    }
+  }
+}
